8c2d47ff2b8f7b5c9fb39a6d8a7930bd2fd521b9
Aly W <alyw131@gmail.com>
2021-12-15 04:24:15 -0800

    .

--- content.js
+++ content.js
@@ -5,13 +5,13 @@ if (pdf) {
   var page_title = document.title;
 
   document.documentElement.innerHTML = '';
-  document.body.style = 'padding: 0px; margin: 0px; width: 100vw; height: 100vh;'
+  document.body.style = 'padding: 0px; margin: 0px; width: 100vw; height: 100vh; overflow: hidden;'
   document.title = page_title;
 
   var embed = document.createElement('embed');
   embed.src = pdf_url;
   embed.type = 'application/pdf';
-  embed.style = 'width: 100vw; height: 100vh; border: none; overflow: hidden;';
+  embed.style = 'width: 100vw; height: 100vh; border: none;';
   document.body.appendChild(embed);
 }
 
